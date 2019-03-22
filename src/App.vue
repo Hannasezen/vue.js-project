67e673b3c71e529c350e63684ff11fa2e8865f32
Hanna Sezen <andreeva0649@gmail.com>
2019-03-22 23:07:32 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>First title {{ msg }}</h1>
+    <Comp v-for="item in arr" :msg="item" :key="item.id"></Comp>
+  </div>
+</template>
+
+<script>
+  import Comp from './Comp.vue'
+
+  export default {
+    name: 'app',
+    data () {
+      return {
+        msg: 'message',
+        arr: [
+          'one', 'two', 'three', 'four'
+        ]
+      }
+    },
+    components: {
+      Comp
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/Comp.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="comp">
+    Comp template <h2>1 {{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'comp',
+  data() {
+    return {
+      item: "item string"
+    }
+  },
+  props: {
+    msg: {
+      type: String,
+      default: "default string"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
